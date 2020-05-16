1d551ad185e6860c24c22a2ecdac7d8ed4b3ac4a
alexwitt2399 <awitt2399@utexas.edu>
2020-05-16 12:15:26 -0500

    fix typo

--- train/train_clf.py
+++ train/train_clf.py
@@ -44,7 +44,7 @@ def train(model_cfg: dict, train_cfg: dict, save_dir: pathlib.Path = None) -> No
         clf_model.cuda()
 
     optimizer = create_optimizer(train_cfg["optimizer"], clf_model)
-    opt = SWA(optimizer, swa_start=0, swa_lr=0.000001)
+    opt = SWA(optimizer, swa_start=0, swa_lr=1e-5)
     epochs = train_cfg.get("epochs", 0)
     assert epochs > 0, "Please supply epoch > 0"
 
@@ -93,7 +93,7 @@ def train(model_cfg: dict, train_cfg: dict, save_dir: pathlib.Path = None) -> No
             f"Epoch: {epoch}, Training loss {sum(all_losses) / len(all_losses):.5}, "
             f"Eval accuracy: {eval_acc:.4}"
         )
-    v
+    
 
 
 def eval(
